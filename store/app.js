44cc1bed1eff62fa729b536b486e3dcfbc75b2fd
Rob Smittenaar <info@robsmittenaar.com>
2023-01-07 23:08:05 +0100

    Remove robots.txt

--- store/app.js
+++ store/app.js
@@ -1,41 +1,41 @@
-export const state = () => ({
-  windowSize: {
-    width: 0,
-    height: 0,
-  },
-  theme: 'dark',
-  menuOpen: false,
-});
+// export const state = () => ({
+//   windowSize: {
+//     width: 0,
+//     height: 0,
+//   },
+//   theme: 'dark',
+//   menuOpen: false,
+// });
 
-export const actions = {
-  setWindowSize({ commit, dispatch }, size) {
-    commit('SET_WINDOW_SIZE', size);
-  },
+// export const actions = {
+//   setWindowSize({ commit, dispatch }, size) {
+//     commit('SET_WINDOW_SIZE', size);
+//   },
 
-  setTheme({ commit }, theme) {
-    commit('SET_THEME', theme);
-  },
+//   setTheme({ commit }, theme) {
+//     commit('SET_THEME', theme);
+//   },
 
-  toggleMenuOpen({ commit }, bool) {
-    commit('TOGGLE_MENU_OPEN', bool);
-  },
-};
+//   toggleMenuOpen({ commit }, bool) {
+//     commit('TOGGLE_MENU_OPEN', bool);
+//   },
+// };
 
-export const mutations = {
-  SET_WINDOW_SIZE(state) {
-    if (process.client) {
-      state.windowSize = {
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    }
-  },
+// export const mutations = {
+//   SET_WINDOW_SIZE(state) {
+//     if (process.client) {
+//       state.windowSize = {
+//         width: window.innerWidth,
+//         height: window.innerHeight,
+//       };
+//     }
+//   },
 
-  SET_THEME(state, val) {
-    state.theme = val;
-  },
+//   SET_THEME(state, val) {
+//     state.theme = val;
+//   },
 
-  TOGGLE_MENU_OPEN(state, payload) {
-    state.menuOpen = payload || !state.menuOpen;
-  }
-};
+//   TOGGLE_MENU_OPEN(state, payload) {
+//     state.menuOpen = payload || !state.menuOpen;
+//   }
+// };
--- store/globalData.js
+++ store/globalData.js
@@ -1,33 +1,33 @@
-export const state = () => ({
-  menu: [],
-  footer: [],
-  options: [],
-});
+// export const state = () => ({
+//   menu: [],
+//   footer: [],
+//   options: [],
+// });
 
-export const actions = {
-  setMenu({ commit }, data) {
-    commit('SET_MENU', data);
-  },
+// export const actions = {
+//   setMenu({ commit }, data) {
+//     commit('SET_MENU', data);
+//   },
 
-  setFooter({ commit }, data) {
-    commit('SET_FOOTER', data);
-  },
+//   setFooter({ commit }, data) {
+//     commit('SET_FOOTER', data);
+//   },
 
-  setOptions({ commit }, data) {
-    commit('SET_OPTIONS', data);
-  },
-};
+//   setOptions({ commit }, data) {
+//     commit('SET_OPTIONS', data);
+//   },
+// };
 
-export const mutations = {
-  SET_MENU(state, val) {
-    state.menu = val;
-  },
+// export const mutations = {
+//   SET_MENU(state, val) {
+//     state.menu = val;
+//   },
 
-  SET_FOOTER(state, val) {
-    state.footer = val;
-  },
+//   SET_FOOTER(state, val) {
+//     state.footer = val;
+//   },
 
-  SET_OPTIONS(state, val) {
-    state.options = val;
-  },
-};
+//   SET_OPTIONS(state, val) {
+//     state.options = val;
+//   },
+// };
