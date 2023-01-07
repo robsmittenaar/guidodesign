daf4dc909ddef054d0b74c30fb26bb72f86152a0
Rob Smittenaar <info@robsmittenaar.com>
2023-01-07 22:44:25 +0100

    Add App Header / Footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
 <template>
   <div ref="app" class="app">
+    <AppHeader />
     <Nuxt />
+    <AppFooter />
   </div>
 </template>
+
+<script>
+import AppFooter from '~/components/appFooter/AppFooter';
+import AppHeader from '~/components/appHeader/AppHeader';
+
+export default {
+  name: 'DefaultLayout',
+
+  components: {
+    AppFooter,
+    AppHeader
+  },
+};
+</script>
