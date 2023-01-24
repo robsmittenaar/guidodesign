e9c28a32ca640ac60d95aa764bccbe491a2e8782
Rob Smittenaar <info@robsmittenaar.com>
2023-01-24 20:51:59 +0100

    Use ipx for nuxt/image

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,10 +47,10 @@ export default {
 	},
 
 	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-	buildModules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module', '@nuxtjs/style-resources', '@nuxt/image'],
+	buildModules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module', '@nuxtjs/style-resources'],
 
 	// Modules: https://go.nuxtjs.dev/config-modules
-	modules: ['@nuxtjs/svg', '@nuxtjs/device'],
+	modules: ['@nuxtjs/svg', '@nuxtjs/device', '@nuxt/image'],
 
 	// Nuxt Router: custom classes
 	router: {
